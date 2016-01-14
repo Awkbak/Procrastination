db4f00d335de71d2316089f3396fe22e6431d027
Bobjrsenior <bobjrsenior@gmail.com>
2016-01-14 12:34:03 -0500

    Created cubicle prefab from table and chair. Also made draggable script that other can inherit from which lets them be dragged with the moust

---
+++ Procrastination/Assets/Scripts/Draggable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Draggable : MonoBehaviour {
+
+    void OnMouseDrag()
+    {
+        Vector3 newPos = Input.mousePosition;
+        newPos.z = 10;
+        newPos = Camera.main.ScreenToWorldPoint(newPos);
+        newPos.y = 0;
+        transform.position = newPos;
+    }
+}
