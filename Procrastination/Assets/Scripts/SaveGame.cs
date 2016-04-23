d3c93628001f2e82a6e8509de7706599a58cdd34
mcosta1 <mcosta1@uncc.edu>
2016-04-23 15:50:20 -0400

    SaveGame and Structure

---
+++ Procrastination/Assets/Scripts/SaveGame.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.IO;
+using System.Collections;
+
+public class Save : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
